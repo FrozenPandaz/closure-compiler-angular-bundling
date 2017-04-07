a2268532ad7bb31a49948a9eb2d921183868e662
Jason Jean <jjean@forbes.com>
2017-04-07 00:49:01 -0400

    Make it work

--- compress.sh
+++ compress.sh
@@ -4,11 +4,6 @@ JVM_ARGS=""
 # To attach a remote debugger:
 #JVM_ARGS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 
-java $JVM_ARGS -jar node_modules/google-closure-compiler/compiler.jar\
-  --flagfile closure.conf \
-  $(find vendor/built/rxjs -name *.js) \
-  built/*.js
-
 # measure the sizes of scripts the user will need to load
 for script in dist/bundle.js node_modules/zone.js/dist/zone.min.js; do
   gzip --keep -f $script
